9cfbbeed5126ae1cb017d4c899b6187b84965557
rleungx <rleungx@gmail.com>
2018-06-10 19:56:52 +0800

    support using bytes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 extern crate ansi_term;
 #[macro_use]
 extern crate clap;
+extern crate regex;
 
 use ansi_term::Colour::{Green, Red};
 use clap::{App, Arg};
+use regex::Regex;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Error};
+use std::io::{self, BufRead, BufReader, Error, Read};
 use std::process::exit;
+
+const UNIT: u64 = 1;
+const DATA_MAGNITUDE: u64 = 1024;
+const KB: u64 = UNIT * DATA_MAGNITUDE;
+const MB: u64 = KB * DATA_MAGNITUDE;
+const GB: u64 = MB * DATA_MAGNITUDE;
+const TB: u64 = (GB as u64) * (DATA_MAGNITUDE as u64);
+const PB: u64 = (TB as u64) * (DATA_MAGNITUDE as u64);
+
+#[derive(Debug, Copy, Clone)]
+enum Mode {
+    Bytes(u64),
+    Lines(i32),
+}
 
 fn main() -> Result<(), Error> {
     let matches = App::new(crate_name!())
         .version(crate_version!())
-        .about("Does awesome things")
+        .about("Yet another head")
         .arg(
             Arg::with_name("FILE")
                 .help("file(s) to print.")
@@ -24,6 +40,13 @@ fn main() -> Result<(), Error> {
                 .short("n")
                 .long("lines")
                 .help("print the first n lines")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("print the first n bytes")
                 .takes_value(true),
         )
         .arg(
@@ -49,24 +72,58 @@ fn main() -> Result<(), Error> {
         })
         .unwrap_or_else(|| vec![None]);
 
-    let lines = matches
-        .value_of("lines")
-        .unwrap_or("10")
-        .parse::<i32>()
-        .unwrap_or_else(|e| {
-            eprintln!("{}", e);
+    let mode = if matches.is_present("lines") {
+        let lines = matches
+            .value_of("lines")
+            .unwrap_or("10")
+            .parse::<i32>()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", e);
+                exit(1);
+            });
+        Mode::Lines(lines)
+    } else {
+        let bytes = matches.value_of("bytes").unwrap();
+        let re = Regex::new(r"([1-9]\d*)([A-Za-z]+)").unwrap();
+        let caps = re.captures(bytes).unwrap_or_else(|| {
+            eprintln!("require a number of bytes");
             exit(1);
         });
+        let byte = caps
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", e);
+                exit(1);
+            }) as u64;
+
+        let unit = match caps.get(2).unwrap().as_str().to_uppercase().as_str() {
+            "K" | "KB" => KB,
+            "M" | "MB" => MB,
+            "G" | "GB" => GB,
+            "T" | "TB" => TB,
+            "P" | "PB" => PB,
+            "B" => UNIT,
+            _ => {
+                eprintln!("only B, KB, MB, GB, TB, PB are supported");
+                exit(1);
+            }
+        };
+        Mode::Bytes(unit * byte)
+    };
 
     let quiet = matches.is_present("quiet");
 
     for file in files {
-        head(file, lines, quiet)?;
+        head(file, mode, quiet)?;
+        println!();
     }
     Ok(())
 }
 
-fn head(file: Option<&str>, lines: i32, quiet: bool) -> Result<(), Error> {
+fn head(file: Option<&str>, mode: Mode, quiet: bool) -> Result<(), Error> {
     let stdin = io::stdin();
     let reader: Box<BufRead> = match file {
         None => Box::new(stdin.lock()),
@@ -77,17 +134,28 @@ fn head(file: Option<&str>, lines: i32, quiet: bool) -> Result<(), Error> {
             Box::new(BufReader::new(File::open(filename)?))
         }
     };
-    let mut line_num = 1;
-    for line in reader.lines() {
-        if line_num <= lines {
-            let l = line.unwrap();
-            if quiet {
-                println!("{}", l);
-            } else {
-                println!("{}: {}", Green.paint(format!("{}", line_num)), l);
+
+    match mode {
+        Mode::Bytes(bytes) => {
+            for byte in reader.bytes().take(bytes as usize) {
+                print!("{}", byte.unwrap() as char);
             }
-            line_num += 1;
+        }
+        Mode::Lines(lines) => {
+            let mut line_num = 1;
+            for line in reader.lines() {
+                if line_num <= lines {
+                    let l = line.unwrap();
+                    if quiet {
+                        println!("{}", l);
+                    } else {
+                        println!("{}: {}", Green.paint(format!("{}", line_num)), l);
+                    }
+                    line_num += 1;
+                }
+            }
         }
     }
+
     Ok(())
 }
